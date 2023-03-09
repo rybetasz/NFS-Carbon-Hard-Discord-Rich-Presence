bf01c1c8f72f5c4d0b6c39f901486031233031b6
rybetasz <108737692+rybetasz@users.noreply.github.com>
2023-03-09 16:14:52 +0300

    Add files via upload

---
+++ races.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <map>
+#include <string>
+
+std::map<std::string, const char*> CIRCUIT_MAP = {
+	{ "STEYNE AQUEDUCT", "game_logo" },
+	{ "BEACHFRONT", "game_logo" },
+	{ "KEMPTON DOCKS", "game_logo" },
+	{ "DOVER STREET", "game_logo" },
+	{ "BROOKS STREET", "game_logo" },
+	{ "NORTH BROADWAY", "game_logo" },
+	{ "SAVANNAH STREET", "game_logo" },
+	{ "PARADISE HOTEL", "game_logo" },
+	{ "UNIVERSITY WAY", "game_logo" },
+	{ "LIBRARY SQUARE", "game_logo" },
+	{ "SKYLINE AVENUE", "game_logo" },
+	{ "OCEAN VIEW", "game_logo" },
+	{ "CONDO ROW", "game_logo" },
+	{ "MAIN STREET", "game_logo" },
+	{ "SUTHERDEN BELL TOWER", "game_logo" },
+	{ "SILK ROAD", "game_logo" },
+	{ "NORTH ROAD", "game_logo" },
+	{ "GARDEN BOULEVARD", "game_logo" },
+	{ "SALAZAR STREET", "game_logo" },
+	{ "ROYAL AVENUE", "game_logo" },
+	{ "SOUTH FORTUNA", "game_logo" },
+	{ "LUCKY TOWERS", "game_logo" },
+	{ "CAMPBELL TUNNEL", "game_logo" },
+	{ "ROBINSON BAY", "game_logo" }
+};
+
+std::map<std::string, const char*> SPRINT_MAP = {
+	{ "DOVER AND LEPUS", "game_logo" },
+	{ "PETERSBURG DAM", "game_logo" },
+	{ "MISSION STREET", "game_logo" },
+	{ "WATERFRONT ROAD", "game_logo" },
+	{ "DESPERATION RIDGE", "game_logo" },
+	{ "SILVERTON WAY", "game_logo" },
+	{ "DEADFALL JUNCTION", "game_logo" },
+	{ "STARLIGHT STREET", "game_logo" },
+	{ "EAGLE DRIVE", "game_logo" },
+	{ "SPADE STREET", "game_logo" },
+	{ "VERONA TUNNEL", "game_logo" },
+	{ "YORK ROAD", "game_logo" },
+	{ "AGOSTINI AVENUE", "game_logo" },
+	{ "NORTH BELLEZZA", "game_logo" },
+	{ "LOFTHY HEIGHTS DOWNHILL", "game_logo" },
+	{ "LINCOLN BOULEVARD", "game_logo" },
+	{ "CHINATOWN TRAM", "game_logo" },
+	{ "GOLD VALLEY RUN", "game_logo" },
+	{ "DEVIL'S CREEK PASS", "game_logo" },
+	{ "MASON STREET", "game_logo" },
+	{ "COPPER RIDGE", "game_logo" },
+	{ "MASON ST BRIDGE", "game_logo" },
+	{ "FRANCIS TUNNEL", "game_logo" },
+	{ "ETERNITY PASS", "game_logo" },
+	{ "JOURNEYMAN'S BANE", "game_logo" },
+	{ "KNIFE'S EDGE", "game_logo" },
+	{ "MESA BRIDGE", "game_logo" },
+	{ "BLACKWELL BRIDGE", "game_logo" },
+	{ "LOOKOUT POINT", "game_logo" }
+};
+
+std::map<std::string, const char*> CHECKPOINT_MAP = {
+	{ "MORGAN BEACH OFFRAMP", "game_logo" },
+	{ "ESKURI PLAZA", "game_logo" },
+	{ "BLACKWALL ROAD", "game_logo" },
+	{ "THE NEON MILE", "game_logo" },
+	{ "TROY", "game_logo" },
+	{ "GRAY STREET", "game_logo" },
+	{ "UNIVERSITY AVENUE", "game_logo" },
+	{ "RABINOWITZ DRIVE", "game_logo" },
+	{ "HILLS BOROUGH PARKWAY", "game_logo" },
+	{ "KNIGHT STREET", "game_logo" }
+};
+
+std::map<std::string, const char*> SPEEDTRAP_MAP = {
+	{ "ESKURI WAY", "game_logo" },
+	{ "STONEWALL TUNNEL", "game_logo" },
+	{ "NEWPORT TRAINYARD", "game_logo" },
+	{ "FOUNDRY ROAD", "game_logo" },
+	{ "CANMOR ROAD", "game_logo" },
+	{ "OLYMPIC BOULEVARD", "game_logo" },
+	{ "BOWEN AVENUE", "game_logo" },
+	{ "MILLION DOLLAR DRIVE", "game_logo" },
+	{ "PARK DRIVE", "game_logo" },
+	{ "MASON FOUNTAIN", "game_logo" }
+}; 
+
+std::map<std::string, const char*> DRIFT_MAP = {
+	{ "KNIFE'S EDGE", "game_logo" },
+	{ "DEVIL'S CREAK PASS", "game_logo" },
+	{ "JOURNEYMAN'S BANE", "game_logo" },
+	{ "CITY COURTHOUSE", "game_logo" },
+	{ "LOFTY HEIGHTS DOWNHILL", "game_logo" },
+	{ "KIMEI TEMPLE", "game_logo" },
+	{ "KINGS PARK", "game_logo" },
+	{ "LOOKOUT POINT", "game_logo" },
+	{ "STARLIGHT STRIP", "game_logo" },
+	{ "OLD QUARTER", "game_logo" },
+	{ "GOLD VALLEY RUN", "game_logo" },
+	{ "NEWPORT IRONWORKS", "game_logo" },
+	{ "FORTUNA HEIGHTS", "game_logo" },
+	{ "PALMONT UNIVERSITY", "game_logo" },
+	{ "BILLINGS DISTRICT", "game_logo" },
+	{ "DESPERATION RIDGE", "game_logo" },
+	{ "PARK DRIVE DRIFT", "game_logo" },
+	{ "DEADFALL JUNCTION", "game_logo" },
+	{ "SHADY PINE", "game_logo" },
+	{ "COPPER RIDGE", "game_logo" },
+	{ "MAIN STREET DRIFT", "game_logo" },
+	{ "ETERNITY PASS", "game_logo" }
+};
+
+std::map<std::string, const char*> CANYON_MAP = {
+	{ "DEADFALL JUNCTION", "game_logo" },
+	{ "DESPERATION RIDGE", "game_logo" },
+	{ "JOURNEYMAN'S BANE", "game_logo" },
+	{ "COPPER RIDGE", "game_logo" },
+	{ "GOLD VALLEY RUN", "game_logo" },
+	{ "KNIFE'S EDGE", "game_logo" },
+	{ "DEVIL'S CREEK PASS", "game_logo" },
+	{ "ETERNITY PASS", "game_logo" },
+	{ "LOOKOUT POINT", "game_logo" },
+	{ "LOFTY HEIGHTS DOWNHILL", "game_logo" }
+};
+
+std::map<std::string, const char*> TAG_MAP = {
+	{ "FORTUNA", "game_logo" },
+	{ "KEMPTON", "game_logo" },
+	{ "DOWNTOWN", "game_logo" },
+	{ "SAN JUAN", "game_logo" },
+	{ "SILVERTON", "game_logo" }
+};
+
+std::map<std::string, const char*> KNOCKOUT_MAP = {
+	{ "FORTUNA", "game_logo" },
+	{ "KEMPTON", "game_logo" },
+	{ "DOWNTOWN", "game_logo" },
+	{ "SAN JUAN", "game_logo" },
+	{ "SILVERTON", "game_logo" }
+};
